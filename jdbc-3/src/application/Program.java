4e57b848bd43206fd58a0d73704d951b449b95d8
Jhoan Victor <jhoanvic.melo@gmail.com>
2020-12-26 15:21:41 -0300

    DEMO - Inserir dados (não retornando id do inserido)

---
+++ jdbc-3/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?,?,?,?,?)");
+
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+
+			/*
+			 * Utilizar o java.sql.Date para instanciar datas que vão para o jdbc e
+			 * inseridas no banco ao passar dentro da instancia um simpleDateFormta .parse,
+			 * usar o método getTime
+			 */
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 2135.0);
+			st.setInt(5, 4);
+
+			// operação que irá alterar dados
+			int rowsAffected = st.executeUpdate();
+
+			System.out.println("Done! Rows Affected: " + rowsAffected);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+	}
+
+}
